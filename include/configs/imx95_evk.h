da6568d34a094cdf26013b2d0628a65501f3af7a
FrancescoFerraro <francesco.f@variscite.com>
2024-06-07 08:06:43 +0200

    imx95_var_dart: add initial Variscite support
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- include/configs/imx95_evk.h
+++ include/configs/imx95_evk.h
@@ -31,17 +31,6 @@
 #define BOOTENV
 #endif
 
-#define JH_ROOT_DTB "imx95-19x19-evk-root.dtb"
-
-#define JAILHOUSE_ENV \
-	"jh_root_dtb=" JH_ROOT_DTB "\0" \
-	"jh_mmcboot=setenv fdtfile ${jh_root_dtb}; " \
-		    "setenv jh_clk clk_ignore_unused mem=1408MB kvm-arm.mode=nvhe; " \
-		    "if run loadimage; then run mmcboot;" \
-		    "else run jh_netboot; fi; \0" \
-	"jh_netboot=setenv fdtfile ${jh_root_dtb}; " \
-		    "setenv jh_clk clk_ignore_unused mem=1408MB kvm-arm.mode=nvhe; run netboot; \0 "
-
 #define CFG_MFG_ENV_SETTINGS \
 	CFG_MFG_ENV_SETTINGS_DEFAULT \
 	"initrd_addr=0x93800000\0" \
@@ -81,7 +70,6 @@
             "\0" \
 /* Initial environment variables */
 #define CFG_EXTRA_ENV_SETTINGS		\
-	JAILHOUSE_ENV \
 	CFG_MFG_ENV_SETTINGS \
 	XEN_BOOT_ENV \
 	BOOTENV \
@@ -89,7 +77,8 @@
 	"prepare_mcore=setenv mcore_clk clk-imx95.mcore_booted;\0" \
 	"scriptaddr=0x93500000\0" \
 	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"image=Image\0" \
+	"image=Image.gz\0" \
+	"img_addr=0xB0000000\0" \
 	"splashimage=0xA0000000\0" \
 	"console=ttyLP0,115200 earlycon\0" \
 	"fdt_addr_r=0x93000000\0"			\
@@ -97,24 +86,33 @@
 	"fdt_high=0xffffffffffffffff\0"		\
 	"cntr_addr=0xA8000000\0"			\
 	"cntr_file=os_cntr_signed.bin\0" \
+	"bootdir=/boot\0" \
+	"fdt_file=undefined\0" \
 	"boot_fit=no\0" \
-	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
 	"bootm_size=0x10000000\0" \
 	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
 	"mmcpart=1\0" \
 	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
 	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot}\0 " \
-	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
+	"optargs=setenv bootargs ${bootargs} ${kernelargs};\0" \
+	"mmcargs=setenv bootargs ${jh_clk} console=${console} \
+		root=/dev/mmcblk${mmcblk}p${mmcpart} rootwait rw\0 " \
+	"loadbootscript=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${fdtfile}\0" \
+	"loadbootenv=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${bootenv}\0" \
+	"importbootenv=echo Importing environment from mmc ...; " \
+		"env import -t -r $loadaddr $filesize\0" \
+	"loadimage=load mmc ${mmcdev}:${mmcpart} ${img_addr} ${bootdir}/${image};" \
+		"unzip ${img_addr} ${loadaddr}\0" \
+	"findfdt=setenv fdt_file imx95-var-dart-dt8mcustomboard.dtb;\0" \
+	"loadfdt=run findfdt;load mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${bootdir}/${fdt_file}\0" \
 	"loadcntr=fatload mmc ${mmcdev}:${mmcpart} ${cntr_addr} ${cntr_file}\0" \
 	"auth_os=auth_cntr ${cntr_addr}\0" \
 	"boot_os=booti ${loadaddr} - ${fdt_addr_r};\0" \
 	"mmcboot=echo Booting from mmc ...; " \
 		"run mmcargs; " \
+		"run optargs; " \
 		"if test ${sec_boot} = yes; then " \
 			"if run auth_os; then " \
 				"run boot_os; " \
@@ -132,11 +130,12 @@
 				"fi; " \
 			"fi;" \
 		"fi;\0" \
-	"netargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} " \
+		"netargs=setenv bootargs ${jh_clk} console=${console} " \
 		"root=/dev/nfs " \
 		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
 	"netboot=echo Booting from net ...; " \
 		"run netargs;  " \
+		"run optargs; " \
 		"if test ${ip_dyn} = yes; then " \
 			"setenv get_cmd dhcp; " \
 		"else " \
@@ -150,11 +149,12 @@
 				"echo ERR: failed to authenticate; " \
 			"fi; " \
 		"else " \
-			"${get_cmd} ${loadaddr} ${image}; " \
+			"${get_cmd} ${img_addr} ${image}; unzip ${img_addr} ${loadaddr}; " \
 			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
 				"bootm ${loadaddr}; " \
 			"else " \
-				"if ${get_cmd} ${fdt_addr_r} ${fdtfile}; then " \
+				"run findfdt; " \
+				"if ${get_cmd} ${fdt_addr_r} ${fdt_file}; then " \
 					"run boot_os; " \
 				"else " \
 					"echo WARN: Cannot load the DT; " \
@@ -163,16 +163,23 @@
 		"fi;\0" \
 	"bsp_bootcmd=echo Running BSP bootcmd ...; " \
 		"mmc dev ${mmcdev}; if mmc rescan; then " \
+"echo 11111111111111; " \
 		   "if run loadbootscript; then " \
+"echo 2222222222222; " \
 			   "run bootscript; " \
 		   "else " \
+"echo 333333333333; " \
 			   "if test ${sec_boot} = yes; then " \
+"echo 4444444444444; " \
 				   "if run loadcntr; then " \
+"echo 5555555555555; " \
 					   "run mmcboot; " \
 				   "else run netboot; " \
 				   "fi; " \
 			    "else " \
+"echo 66666666666666; " \
 				   "if run loadimage; then " \
+"echo 7777777777777; " \
 					   "run mmcboot; " \
 				   "else run netboot; " \
 				   "fi; " \
